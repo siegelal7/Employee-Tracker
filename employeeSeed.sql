2e182b1b2bad780a1ce695a7669f17557f16e0a7
Andrew Siegel <siegelal7@gmail.com>
2020-10-03 18:28:53 -0400

    messing with inner join and setting aliases mysql

--- employeeSeed.sql
+++ employeeSeed.sql
@@ -14,7 +14,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2),
-    departmentId INT NOT NULL,
+    departmentId INT NULL,
     PRIMARY KEY (id)
 );
 
@@ -26,3 +26,23 @@ CREATE TABLE employee (
     managerId INT NULL,
     PRIMARY KEY(id)
 );
+-- TODO: delete all this tester stuff
+SELECT e.firstName, r.title, r.salary, d.departmentName, em.firstName AS "manager"
+FROM employee e
+LEFT JOIN employee em ON em.id = e.managerId
+INNER JOIN role r ON e.roleId = r.id
+INNER JOIN department d ON r.departmentId = d.id;
+
+INSERT INTO department (departmentName) VALUES ("HR");
+INSERT INTO role (title,salary, departmentId) VALUES ("Imminent Archon", 75000.23, 1);
+INSERT INTO employee (firstName, lastName, roleId, managerId) VALUES ("Andrew", "Siegel", 2, 0);
+INSERT INTO employee (firstName, lastName, roleId, managerId) VALUES ("Tester", "Employee", 1, 1);
+SELECT * FROM role;
+
+INSERT INTO role (title,salary, departmentId) VALUES ("Test role", 23423.00, 1);
+
+SELECT role.title, role.salary, department.departmentName
+FROM role
+INNER JOIN department ON role.departmentId = department.id;
+
+
