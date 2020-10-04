d14c3e15215f6ed1d45cfeb30094feba4e021c04
Andrew Siegel <siegelal7@gmail.com>
2020-10-04 00:19:59 -0400

    got db all set up and playing with join commands

--- app.js
+++ app.js
@@ -1,2 +1,168 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const roles = [
+  "Sales Lead",
+  "Salesperson",
+  "Lead Engineer",
+  "Software Engineer",
+  "Account Manager",
+  "Accountant",
+  "Legal Team Lead",
+  "Lawyer",
+];
+const departments = ["Finance", "Engineering", "Sales", "Legal"];
+
+const employees = [];
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Gobraves",
+  database: "employeesDB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadId);
+});
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "action",
+        choices: [
+          "View all employees",
+          "View all employees by department",
+          "View all employees by manager",
+          "Add employee",
+          "Remove employee",
+          "Update employee role",
+          "Update employee manager",
+        ],
+      },
+    ])
+    .then(function (data) {
+      determineAction(data);
+    });
+}
+
+const determineAction = (data) => {
+  //   console.table(data);
+  if (data.action === "View all employees") {
+    viewAll();
+  } else if (data.action === "View all employees by department") {
+    //TODO: figure out how to do this function
+    viewByDept();
+  } else if (data.action === "View all employees by manager") {
+    //TODO: figure out how to do this function
+    console.log("f");
+  } else if (data.action === "Add employee") {
+    //TODO: figure out how to do this function
+    addEmployee();
+  } else if (data.action === "Remove employee") {
+    //TODO: figure out how to do this function
+    console.log("f");
+  } else if (data.action === "Update employee role") {
+    //TODO: figure out how to do this function
+    console.log("f");
+  } else if (data.action === "Update employee manager") {
+    //TODO: figure out how to do this function
+    console.log("f");
+  }
+};
+
+const viewAll = () => {
+  //   connection.query(
+  //     `SELECT e.id, e.firstName, r.title, r.salary, d.departmentName, em.firstName AS "manager"
+  //     FROM employee e
+  //     LEFT JOIN employee em ON em.id = e.managerId
+  //     INNER JOIN role r ON e.roleId = r.id
+  //     INNER JOIN department d ON r.departmentId = d.id`,
+  //     function (err, res) {
+  //       if (err) throw err;
+  //       console.table(res);
+  //     }
+  //   );
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+  });
+};
+
+const viewByDept = () => {
+  connection.query(`SELECT e.id, e.firstName, e.lastName, d.departmentName
+  FROM employee e
+  INNER JOIN department d
+  ON e.roleId = d.id;`);
+};
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter employee first name:",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "Enter employee last name:",
+        name: "lastName",
+      },
+      {
+        type: "list",
+        message: "What is the new employee's role?",
+        choices: roles,
+        name: "role",
+      },
+      //   {
+      //     type: "list",
+      //     message: "Who is the employee's manager?",
+      //     choices: employees,
+      //     name: "manager",
+      //   },
+    ])
+    .then(function (data) {
+      employees.push(`${data.firstName} ${data.lastName}`);
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          roleId: determineRoleId(data),
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " employee inserted\n");
+          init();
+        }
+      );
+    });
+};
+function determineRoleId(data) {
+  if (data.role === "Sales Lead") {
+    return 1;
+  } else if (data.role === "Salesperson") {
+    return 2;
+  } else if (data.role === "Lead Engineer") {
+    return 3;
+  } else if (data.role === "Software Engineer") {
+    return 4;
+  } else if (data.role === "Account Manager") {
+    return 5;
+  } else if (data.role === "Accountant") {
+    return 6;
+  } else if (data.role === "Legal Team Lead") {
+    return 7;
+  } else if (data.role === "Lawyer") {
+    return 8;
+  }
+}
+
+init();
--- employeeSeed.sql
+++ employeeSeed.sql
@@ -22,27 +22,46 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(30) NOT NULL,
     lastName VARCHAR(30) NOT NULL,
-    roleId INT NOT NULL,
+    roleId INT NULL,
     managerId INT NULL,
     PRIMARY KEY(id)
 );
--- TODO: delete all this tester stuff
-SELECT e.firstName, r.title, r.salary, d.departmentName, em.firstName AS "manager"
+INSERT INTO role (title,salary, departmentId) VALUES ("Salesperson", 52000.00, 1);
+INSERT INTO role (title,salary, departmentId) VALUES ("Sales Lead", 61000.00, 1);
+INSERT INTO role (title,salary, departmentId) VALUES ("Lead Engineer", 100000.00, 4);
+INSERT INTO role (title,salary, departmentId) VALUES ("Software Engineer", 72000.00, 4);
+INSERT INTO role (title,salary, departmentId) VALUES ("Account Manager", 71000.00, 3);
+INSERT INTO role (title,salary, departmentId) VALUES ("Accountant", 67000.00, 3);
+INSERT INTO role (title,salary, departmentId) VALUES ("Legal Team Lead", 102000.00, 2);
+INSERT INTO role (title,salary, departmentId) VALUES ("Lawyer", 79000.00, 2);
+
+INSERT INTO department (departmentName) VALUES ("Sales");
+INSERT INTO department (departmentName) VALUES ("Legal");
+INSERT INTO department (departmentName) VALUES ("Finance");
+INSERT INTO department (departmentName) VALUES ("Engineering");
+
+SELECT e.id, e.firstName, r.title, r.salary, d.departmentName, em.firstName AS "manager"
 FROM employee e
 LEFT JOIN employee em ON em.id = e.managerId
 INNER JOIN role r ON e.roleId = r.id
 INNER JOIN department d ON r.departmentId = d.id;
 
-INSERT INTO department (departmentName) VALUES ("HR");
-INSERT INTO role (title,salary, departmentId) VALUES ("Imminent Archon", 75000.23, 1);
-INSERT INTO employee (firstName, lastName, roleId, managerId) VALUES ("Andrew", "Siegel", 2, 0);
-INSERT INTO employee (firstName, lastName, roleId, managerId) VALUES ("Tester", "Employee", 1, 1);
-SELECT * FROM role;
+
+INSERT INTO employee (firstName, lastName, roleId) VALUES ("Andrew", "Siegel", 4);
+INSERT INTO employee (firstName, lastName, roleId) VALUES ("Tester", "Employee", 1);
+INSERT INTO employee (firstName, lastName, roleId) VALUES ("Amanda", "Newquist", 8);
+INSERT INTO employee (firstName, lastName, roleId) VALUES ("David", "Siegel", 6);
+
 
 INSERT INTO role (title,salary, departmentId) VALUES ("Test role", 23423.00, 1);
 
-SELECT role.title, role.salary, department.departmentName
+SELECT employee.firstName, employee.lastName, role.title, department.departmentName
 FROM role
-INNER JOIN department ON role.departmentId = department.id;
+INNER JOIN department ON role.departmentId = department.id
+INNER JOIN employee ON role.id = employee.roleId;
 
-
+-- returning emp. if their roleId matches departmentId
+SELECT e.id, e.firstName, e.lastName, d.departmentName
+FROM employee e
+INNER JOIN department d
+ON e.roleId = d.id;
